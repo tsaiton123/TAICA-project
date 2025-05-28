5d280f5edf2c542dd510d239ac714241a592db49
tsaiton123 <tsaiton123@gmail.com>
2025-05-28 21:58:54 +0800

    post

--- app.py
+++ app.py
@@ -20,6 +20,8 @@ database_url    = os.getenv("DATABASE_URL", "sqlite:///cache.db")
 def create_app():
     app = Flask(__name__)
     # --- Database config & init ---
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"]        = database_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
